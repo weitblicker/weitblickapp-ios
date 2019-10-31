d013a38561509e6e20a770d3cbc0e8dabe3d4436
jjtacke <55894350+jjtacke@users.noreply.github.com>
2019-10-31 14:18:58 +0100

    new Detailcontroller

---
+++ Weitblick/Weitblick/Controller/NewsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsDetailViewController.swift
+//  Weitblick
+//
+//  Created by Jana  on 31.10.19.
+//  Copyright © 2019 HS Osnabrueck. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
