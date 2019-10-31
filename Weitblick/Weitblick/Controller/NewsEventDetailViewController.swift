b59f5e43ea10f6f4c669b7e69e83eae1376d6125
jjtacke <55894350+jjtacke@users.noreply.github.com>
2019-10-31 15:54:08 +0100

    120

--- Weitblick/Weitblick/Controller/NewsEventDetailViewController.swift
+++ Weitblick/Weitblick/Controller/NewsEventDetailViewController.swift
@@ -14,7 +14,7 @@ class NewsEventDetailViewController: UIViewController {
         super.viewDidLoad()
         
         // DIES IST EIN KOMMENTAR
-        let a = 100
+        let a = 120
         let b = 20
         let c = a + b
 
