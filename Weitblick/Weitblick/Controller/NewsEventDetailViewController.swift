e6bf26773e0682d34e56ca8aa32dd4803022eff6
Zuzanna Mielczarek <52738991+zuzanna3101@users.noreply.github.com>
2019-11-04 13:59:59 +0100

    News Event List

--- Weitblick/Weitblick/Controller/NewsEventDetailViewController.swift
+++ Weitblick/Weitblick/Controller/NewsEventDetailViewController.swift
@@ -17,6 +17,19 @@ class NewsEventDetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        let nav = self.navigationController?.navigationBar
+        
+        nav?.barStyle = UIBarStyle.default
+        
+        let imageView = UIImageView(frame : CGRect(x : 0 , y : 0, width : 40 , height : 40))
+        imageView.contentMode = .scaleAspectFit
+        
+        let image = UIImage(named : "Weitblick")
+        imageView.image = image
+        
+        navigationItem.titleView = imageView
+    }
     
 
     /*
