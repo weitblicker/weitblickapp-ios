1e0e1573d1bf6a26d22bdf6dafb44b62789f3943
Michel Jean-Pierre Einsweiler <michel.einsweiler@hs-osnabrueck.de>
2019-11-08 17:15:57 +0100

    Fetch Project Data from REST API

--- Weitblick/Weitblick/Controller/NewsEventViewController.swift
+++ Weitblick/Weitblick/Controller/NewsEventViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
+struct ProjectData : Codable{
+    let id : String?
+    let start_date : String?
+    let end_date : String?
+    let published : String?
+    let name : String?
+    let slug : String?
+    let hosts : [String]?
+    let description : String?
+    let location : Int?
+    let partner : [Int]?
+}
+
+
 class NewsEventViewController: UIViewController {
     
+    var projectList : [Project] = []
     
     
     @IBOutlet weak var NewsView: UIView!
@@ -30,8 +45,74 @@ class NewsEventViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginWithData()
         NewsView.alpha=1
         EventView.alpha=0
+    }
+//    extension String {
+//            func fromBase64() -> String? {
+//                    guard let data = Data(base64Encoded: self) else {
+//                            return nil
+//                    }
+//                    return String(data: data, encoding: .utf8)
+//            }
+//            func toBase64() -> String {
+//                    return Data(self.utf8).base64EncodedString()
+//            }
+//    }
+    
+    private func loginWithData(){
+        //let params = ["username":"surfer", "password":"hangloose"] as Dictionary<String, String>
+
+        let str = "surfer:hangloose"
+        let test2 = Data(str.utf8).base64EncodedString();
+        var request = URLRequest(url: URL(string: "https://new.weitblicker.org/rest/projects/?limit=3")!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic " + test2, forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            let responseJSON = try? JSONSerialization.jsonObject(with: data!, options: [])
+            do {
+                let options = JSONSerialization.WritingOptions.prettyPrinted
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: responseJSON as Any, options: options)
+                    if let string = String(data: data, encoding: String.Encoding.utf8) {
+                        let data = string.data(using: .utf8)!
+                        //print(string)
+                        do {
+                            let f = try JSONDecoder().decode([ProjectData].self, from: data)
+                            //print(f)
+                            for project in f {
+                                guard let id = Int.init(project.id!) else { return }
+                                guard let name = project.name else { return }
+                                guard let hosts = project.hosts else { return }
+                                guard let description = project.description else { return }
+                                guard let locationID = project.location else { return }
+                                let partnerID = project.partner ?? []
+                                let project = Project(id : id,name : name,hosts : hosts,description : description,locationID : locationID,partnerID :partnerID)
+                                self.projectList.append(project)
+                                print("Project added")
+                                print(project.toString)
+                                print("\n")
+                            }
+                            
+                        } catch {
+                            print(error)
+                        }
+                        
+                    }
+                } catch {
+                  print(error)
+                }
+            }
+        })
+        
+
+        task.resume()
+
+        
     }
     
 
