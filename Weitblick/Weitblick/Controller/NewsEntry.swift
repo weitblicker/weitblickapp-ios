5c2f381e4e48c731215c93f459d684c96719a820
Michel Jean-Pierre Einsweiler <michel.einsweiler@hs-osnabrueck.de>
2019-11-12 11:24:49 +0100

    News Loading added, Models updated

--- Weitblick/Weitblick/Controller/NewsEntry.swift
+++ Weitblick/Weitblick/Controller/NewsEntry.swift
@@ -8,22 +8,46 @@
 
 import Foundation
 
+struct Image : Codable{
+    let id : Int?
+    let title : String?
+    let caption : String?
+    let imageURL : String?
+    let date_taken : Date?
+}
+
+struct Gallery : Codable{
+    let id : Int?
+    let title : String?
+    let description : String?
+    let images : [Image]?
+}
+
+struct NewsDecodable : Codable{
+    let id : String?
+    let title : String?
+    let text : String?
+    let gallery : Gallery?
+    let added : String?
+    let updated : String?
+    let range : String?
+}
+
 class NewsEntry{
-    
     private var id : Int
     private var title : String
     private var text : String
-    private var imageID : Int
+    private var gallery : Gallery
     private var created : Date
     private var updated : Date
     private var range : String
 
-    init(id : Int, title : String, text : String, imageID : Int, created : Date, updated : Date, range : String){
+    init(id : Int, title : String, text : String, gallery : Gallery, created : Date, updated : Date, range : String){
         
         self.id = id
         self.title = title
         self.text = text
-        self.imageID = imageID
+        self.gallery = gallery
         self.created = created
         self.updated = updated
         self.range = range
@@ -42,8 +66,12 @@ class NewsEntry{
         return self.text
     }
     
-    public var getImageID : Int{
-        return self.imageID
+    public func getImageURL(index : Int) -> String{
+        if(index >= 0 && index < self.gallery.images!.count){
+            return self.gallery.images![index].imageURL!
+        }else{
+            return "NO_IMAGES_IN_GALLERY_ERROR"
+        }
     }
     
     public var getCreationDate : Date{
