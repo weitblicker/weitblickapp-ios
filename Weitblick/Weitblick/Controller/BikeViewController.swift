c1441642da562b7aa3f1c103b9d94d4417a13e61
Michel Jean-Pierre Einsweiler <michel.einsweiler@hs-osnabrueck.de>
2019-11-23 16:13:54 +0100

    Added Calculating Kilometers and Speed

--- Weitblick/Weitblick/Controller/BikeViewController.swift
+++ Weitblick/Weitblick/Controller/BikeViewController.swift
@@ -9,13 +9,31 @@
 import UIKit
 import MapKit
 import CoreLocation
-
 class BikeViewController: UIViewController {
-
+    var list : [CLLocation] = []
+    var i = 0;
+    var totalDistance : Double = 0;
+    var locationSaved = CLLocation()
+    var startTracking = false;
+    var startCalculateDistance = false;
+    var distance = CLLocationDistance();
     let locationManager = CLLocationManager()
+    let regionInMeters : Double = 1000;
 
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     @IBAction func startDataTracking(_ sender: Any) {
+        
+        if(!startCalculateDistance){
+            startCalculateDistance = true;
+            startTracking = true;
+        }else{
+            startCalculateDistance = false;
+        }
+        
+        
+        
     }
 
     override func viewDidLoad() {
@@ -40,7 +58,7 @@ class BikeViewController: UIViewController {
     
     func centerViewOnUserLocation(){
         if let location = locationManager.location?.coordinate{
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 10000, longitudinalMeters: 10000)
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
             self.mapView.setRegion(region, animated: true)
         }
     }
@@ -52,6 +70,7 @@ class BikeViewController: UIViewController {
             print("AUTHORIZED")
             self.mapView.showsUserLocation = true
             centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
             break
         case .denied:
             print("DENIED")
@@ -69,6 +88,7 @@ class BikeViewController: UIViewController {
             print("AUTHORIZED ALWAYS")
             self.mapView.showsUserLocation = true
             centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
             break
         }
     }
@@ -77,10 +97,45 @@ class BikeViewController: UIViewController {
 extension BikeViewController : CLLocationManagerDelegate{
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // TODO
+        
+        guard let location = locations.last else{ return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        self.mapView.setRegion(region, animated: true)
+        
+        if(startTracking){
+            if(self.startCalculateDistance){
+                self.list.append(location)
+                if(self.list.count >= 2){
+                    let start = self.list[self.list.count-2]
+                    let end = self.list[self.list.count-1]
+                    self.totalDistance += end.distance(from: start)
+                    self.distanceLbl.text = (round(self.totalDistance)/1000).description + " km"
+                    self.speedLbl.text = (round(location.speed * 3.6*1000)/1000).description + "km/h"
+                }
+            }else{
+                var total: Double = 0.0
+                for i in 0..<self.list.count - 1 {
+                    let start = list[i]
+                    let end = list[i + 1]
+                    let distance = end.distance(from: start)
+                    total += distance
+                    
+                }
+                self.list = []
+                let alert = UIAlertController(title: "Distance Calculated", message: total.description + " meters", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+                }))
+                self.present(alert, animated: true, completion: nil)
+                self.startTracking = false
+                self.distanceLbl.text = ""
+                self.totalDistance = 0
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        // TODO
+        checkLocationAuthorization()
     }
 }
