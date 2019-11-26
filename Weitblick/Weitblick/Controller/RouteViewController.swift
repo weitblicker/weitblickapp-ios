7ab6ebe39706074e1bed9addc61c980375bdafdb
jjtacke <55894350+jjtacke@users.noreply.github.com>
2019-11-26 11:30:45 +0100

    add switch between stats and destination view

---
+++ Weitblick/Weitblick/Controller/RouteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RouteViewController.swift
+//  Weitblick
+//
+//  Created by Jana  on 26.11.19.
+//  Copyright © 2019 HS Osnabrueck. All rights reserved.
+//
+
+import UIKit
+
+class RouteViewController: UIViewController {
+
+     override func viewDidLoad() {
+           super.viewDidLoad()
+
+           // Do any additional setup after loading the view.
+       }
+       
+
+}
