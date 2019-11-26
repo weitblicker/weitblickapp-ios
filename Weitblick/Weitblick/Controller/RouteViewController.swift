ed18e19e3348cccd90f14cd42d3262efea9ba236
Zuzanna Mielczarek <52738991+zuzanna3101@users.noreply.github.com>
2019-11-26 19:06:15 +0100

    Route TableView

--- Weitblick/Weitblick/Controller/RouteViewController.swift
+++ Weitblick/Weitblick/Controller/RouteViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class RouteViewController: UIViewController {
+class RouteViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+   
+    @IBOutlet weak var tableView: UITableView!
+    let routes = ["5.4 km","6.2 km", "1.7 km","23.8 km"];
+    let donatations = ["5,5 €", "6,5 €", "2€","24 €"]
+    let times = ["45 min", "52 min", "17 min","46 min"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return routes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier:"route_cell", for: indexPath)as! RouteTableViewCell
+        let route = routes[indexPath.row]
+        let donation = donatations[indexPath.row]
+        let time = times[indexPath.row]
+        cell.route.text = route
+        cell.donation.text = donation
+        cell.time.text = time
+        cell.date.text = "24.12.19"
+        return cell
+        
+    }
+    
 
      override func viewDidLoad() {
            super.viewDidLoad()
+     
+/*        self.tableView.dataSource = self
+        self.tableView.delegate = self*/
 
            // Do any additional setup after loading the view.
        }
        
 
 }
+
