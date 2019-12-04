1716cec0f31fb21a0c6fdcad8f6f1e38b5f55e54
jjtacke <55894350+jjtacke@users.noreply.github.com>
2019-12-04 11:27:45 +0100

    move view wit keyboard

--- Weitblick/Weitblick/Controller/RegisterViewController.swift
+++ Weitblick/Weitblick/Controller/RegisterViewController.swift
@@ -18,7 +18,10 @@ class RegisterViewController: UIViewController {
                   tap.cancelsTouchesInView = false
                 view.addGestureRecognizer(tap)
 
+        //view verschieben bei tastatur
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
 
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
     }
     override func didReceiveMemoryWarning() {
            super.didReceiveMemoryWarning()
@@ -30,6 +33,16 @@ class RegisterViewController: UIViewController {
         view.endEditing(true)
     }
 
+    
+    //view verschieben wenn tastaur erscheint
+       @objc func keyboardWillShow(sender: NSNotification) {
+            self.view.frame.origin.y = -230 // Move view 150 points upward
+       }
+
+       @objc func keyboardWillHide(sender: NSNotification) {
+            self.view.frame.origin.y = 0 // Move view to original position
+       }
+    
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
