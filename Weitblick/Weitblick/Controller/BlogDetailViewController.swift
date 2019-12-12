a798299a6945951f6d72efe1ed4386a82abb1a1e
jjtacke <55894350+jjtacke@users.noreply.github.com>
2019-12-12 12:06:41 +0100

    blog detail fixed half

--- Weitblick/Weitblick/Controller/BlogDetailViewController.swift
+++ Weitblick/Weitblick/Controller/BlogDetailViewController.swift
@@ -35,6 +35,23 @@ class BlogDetailViewController: UIViewController {
     }
     
     
+    override func viewWillAppear(_ animated: Bool) {
+          let nav = self.navigationController?.navigationBar
+
+          nav?.barStyle = UIBarStyle.default
+          
+          let imageView = UIImageView(frame : CGRect(x : 0 , y : 0, width : 40 , height : 40))
+          imageView.contentMode = .scaleAspectFit
+          
+          let image = UIImage(named : "Weitblick")
+          imageView.image = image
+          
+          navigationItem.titleView = imageView
+        
+
+      }
+      
+    
     func loadDetailBlog(){
         
       blog_detail_title.text = blog_object?.getTitle
