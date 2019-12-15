72e00cec876808a082db67fb7c20be1ec2be7389
Michel Jean-Pierre Einsweiler <michel.einsweiler@hs-osnabrueck.de>
2019-12-15 17:12:54 +0100

    Update NewsEntry.swift

--- Weitblick/Weitblick/Controller/NewsEntry.swift
+++ Weitblick/Weitblick/Controller/NewsEntry.swift
@@ -9,74 +9,38 @@
 import Foundation
 import UIKit
 
-struct Image : Codable{
-    let imageURL : String?
-}
-
-struct Gallery : Codable{
-    let images : [Image]?
-}
-
-struct NewsDecodable : Codable{
-    let id : String?
-    let title : String?
-    let text : String?
-    let gallery : Gallery?
-    let added : String?
-    let updated : String?
-    let range : String?
-}
-
 class NewsEntry{
     private var id : Int
     private var title : String
     private var text : String
-    private var image : Image
-    private var uiimage : UIImage = UIImage(named: "Weitblick")!
-    private var gallery : Gallery
+    private var image : UIImage
+    private var gallery : [UIImage]
     private var created : Date
     private var updated : Date
     private var range : String
     private var teaser : String
 
-    init(id : Int, title : String, text : String, gallery : Gallery, created : Date, updated : Date, range : String, image : Image, teaser: String){
+    init(id : Int, title : String, text : String, gallery : [UIImage], created : Date, updated : Date, range : String, image : UIImage, teaser: String){
 
         self.id = id
         self.title = title
         self.text = text
-        self.gallery = gallery
+        
         self.created = created
         self.updated = updated
         self.range = range
         self.image = image
         self.teaser = teaser
         
-        if(self.image.imageURL == ""){
-            if(self.gallery.images!.count >= 1){
-                let imgURL = NSURL(string : Constants.url + (self.gallery.images?.first?.imageURL)! )
-                if(imgURL != nil){
-                    let data = NSData(contentsOf: (imgURL as URL?)!)
-                    let size = CGSize.init(width: 334, height: 176)
-                    let img = UIImage(data: data! as Data)?.crop(to: size)
-                    self.uiimage = img!
-                }
-            }else{
-                self.uiimage = UIImage(named: "Weitblick")!
-            }
-        }else{
-            let imgURL = NSURL(string : Constants.url + self.image.imageURL!)
-            if(imgURL != nil){
-                let data = NSData(contentsOf: (imgURL as URL?)!)
-                self.uiimage = UIImage(data: data! as Data)!
-            }else{
-                self.uiimage = UIImage(named: "Weitblick")!
-            }
-            
-        }
+        let size = CGSize.init(width: 334, height: 176)
+        self.image = self.image.crop(to: size)
+        
+
+        self.gallery = gallery
     }
     
     public var getImage : UIImage{
-        return self.uiimage
+        return self.image
     }
 
     public var getID : Int{
@@ -91,21 +55,10 @@ class NewsEntry{
         return self.text
     }
 
-    public func getImageURLFromGallery(index : Int) -> String{
-        if(index >= 0 && index < self.gallery.images!.count){
-            return self.gallery.images![index].imageURL!
-        }else{
-            return "NO_IMAGES_IN_GALLERY_ERROR"
-        }
-    }
     
-    public var getGallery : Gallery{
+    public var getGallery : [UIImage]{
            return self.gallery
        }
-
-    public var getImageURL : String{
-        return self.image.imageURL!
-    }
 
     public var getCreationDate : Date{
         return self.created
