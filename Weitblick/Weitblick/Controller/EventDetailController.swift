a453fca559f6579d133b87548660aa84409f95aa
Michel Jean-Pierre Einsweiler <michel.einsweiler@hs-osnabrueck.de>
2020-01-27 20:22:41 +0100

    added EventDetailController

---
+++ Weitblick/Weitblick/Controller/EventDetailController.swift
@@ -0,0 +1,30 @@
+//
+//  EventDetailController.swift
+//  Weitblick
+//
+//  Created by Michel Einsweiler on 27.01.20.
+//  Copyright © 2020 HS Osnabrueck. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
