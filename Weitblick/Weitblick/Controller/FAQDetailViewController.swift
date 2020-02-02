0f1723db04c51672b73989d26f5516f6e40c1c38
jjtacke <55894350+jjtacke@users.noreply.github.com>
2020-02-02 20:11:52 +0100

    fay detail

---
+++ Weitblick/Weitblick/Controller/FAQDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FAQDetailViewController.swift
+//  Weitblick
+//
+//  Created by Jana  on 02.02.20.
+//  Copyright © 2020 HS Osnabrueck. All rights reserved.
+//
+
+import UIKit
+
+class FAQDetailViewController: UIViewController {
+
+    @IBOutlet weak var Answer: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
