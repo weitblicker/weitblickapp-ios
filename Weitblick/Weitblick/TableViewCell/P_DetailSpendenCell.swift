e18c6740400034ceb40f8d1b3c518f42118ed246
Zuzanna Mielczarek <52738991+zuzanna3101@users.noreply.github.com>
2020-02-07 15:30:53 +0100

    ProjectDetailCell

--- Weitblick/Weitblick/TableViewCell/P_DetailSpendenCell.swift
+++ Weitblick/Weitblick/TableViewCell/P_DetailSpendenCell.swift
@@ -18,6 +18,9 @@ class P_DetailSpendenCell: UITableViewCell {
     @IBOutlet weak var spendenkonto: UILabel!
     @IBOutlet weak var spendenstand: UILabel!
     
+    @IBOutlet weak var spendenbeschreibung: UILabel!
+    @IBOutlet weak var spenden_star_img: UIImageView!
+    
     @IBOutlet weak var spendenziel: UILabel!
     override func awakeFromNib() {
             super.awakeFromNib()
