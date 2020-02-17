1a71d4a2f79929f3ff7acb7a376110b463748784
jjtacke <55894350+jjtacke@users.noreply.github.com>
2020-02-17 15:18:54 +0100

    layout fixed

--- Weitblick/Weitblick/Controller/ResultMapViewController.swift
+++ Weitblick/Weitblick/Controller/ResultMapViewController.swift
@@ -67,9 +67,9 @@ class ResultMapViewController: UIViewController ,UITableViewDataSource, UITableV
         self.project_location.text = self.project?.getLocation.getAddress
         for host in self.project!.getHosts{
             if(self.project!.getHosts.count > 1){
-            self.hostList = self.hostList + host.getName + ","
+                self.hostList = self.hostList + host.getCity.uppercased() + ","
             }else{
-                self.hostList = self.hostList + host.getName
+                self.hostList = self.hostList + host.getCity.uppercased()
             }
         }
         self.project_city.text = self.hostList
